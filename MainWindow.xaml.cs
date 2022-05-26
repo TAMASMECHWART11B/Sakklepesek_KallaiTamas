a20dc4329b276185ebe4d1099ea428c6aca4efbe
TAMASMECHWART11B <kallaitamas04@gmail.com>
2022-05-26 22:35:27 +0200

    Menüsor
    A bábuk kiválasztásához való Combobox létrehozva benne bábukkal

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,52 @@ namespace Sakklepesek_KállaiTamásMiklós
     /// </summary>
     public partial class MainWindow : Window
     {
+        Rectangle[,] tablaM;
+        Grid tabla;
+        ComboBox valasztas;
         public MainWindow()
         {
             InitializeComponent();
+            FelGeneral();
+            Tabla();
         }
+
+        private void Tabla()
+        {
+
+        }
+
+        private void FelGeneral()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                ablak.RowDefinitions.Add(new RowDefinition());
+
+            }
+            ComboBox vbabu = new ComboBox();
+            vbabu.Items.Add("Király");
+            vbabu.Items.Add("Vezér");
+            vbabu.Items.Add("Futó");
+            vbabu.Items.Add("Bástya");
+            vbabu.Items.Add("Huszár");
+            vbabu.Items.Add("Fekete paraszt");
+            vbabu.Items.Add("Fehér paraszt");
+            ablak.Children.Add(vbabu);
+            Grid.SetRow(vbabu, 0);
+            tabla = new Grid();
+            for (int i = 0; i < 10; i++)
+            {
+                tabla.RowDefinitions.Add(new RowDefinition());
+                tabla.ColumnDefinitions.Add(new ColumnDefinition());
+
+            }
+            tabla.Width = 600;
+            tabla.Height = 600;
+            ablak.Children.Add(tabla);
+            Grid.SetRow(tabla, 1);
+
+        }
+
+
     }
 }
